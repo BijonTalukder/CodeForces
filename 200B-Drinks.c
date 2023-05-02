5c5e56624fbfe12383090c018eeb0766c7aedf60
BijonTalukder <bijontalukder1247@gmail.com>
2023-05-02 10:55:31 +0600

    solved

---
+++ 200B-Drinks.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n ,sum =0;
+    scanf("%d",&n);
+    int t=n;
+    double ans;
+    while(n--){
+            int x;
+    scanf("%d",&x);
+sum= sum+x;
+//printf("%d",sum);
+
+    }
+    printf("%.12f",1.0*sum/t);
+    //printf("%.12f",sum);
+
+
+
+}
