4ad31efdf57f841202bfd8d72d1902192bf4fca0
Bijon <bijontalukder1247@gmail.com>
2025-05-05 02:18:53 +0600

    added queue

--- queueArray.cpp
+++ queueArray.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX_SIZE = 500;
+const int MAX_SIZE = 5;
 class Queue
 {
 public:
@@ -8,19 +8,23 @@ public:
   int l, r;
   Queue()
   {
+
+    cout << "Queue is created\n"<<l<<" "<<r<<" "<<MAX_SIZE<<endl;
     l = 0;
     r = -1;
   }
 
-  void enqueue(int x)
+  // left =0 ,right = -1
+  // cout<<"left = "<<l<<" right = "<<r<<1<<endl;
+  void enqueue(int x)    //x=1, x=6 , x=7 x=9
   {
     if (r + 1 >= MAX_SIZE)
     {
       cout << "Queue is full\n";
       return;
     }
-    r++;
-    a[r] = x;
+    r++; //r-0 , r-1, r-2 r-3 r-4
+    a[r] = x; //a[0]=1 a[1]=6 a[2]=7 a[3]=9
   }
 
   void dequeue()
@@ -30,6 +34,8 @@ public:
       cout << "Queue is empty\n";
       return;
     }
+
+    
     l++;
   }
   int front()
@@ -53,13 +59,23 @@ main()
 {
 
   Queue q;
-  q.enqueue(5);
+  q.enqueue(1);
   q.enqueue(6);
   q.enqueue(7);
-  cout<<q.front()<<" "<<q.size();
+  q.enqueue(9);
+  q.enqueue(16);
+  q.enqueue(16);
+
+
+  cout<<q.front()<<" ";
+  // q.dequeue();
+  cout<<q.front()<<" ";
+  // q.dequeue();
+  cout<<q.front()<<" ";
+  cout<<q.front()<<" ";
+  cout<<q.front()<<" ";
   q.dequeue();
-  cout<<q.front()<<" "<<q.size();
-  q.dequeue();
-  cout<<q.front()<<" "<<q.size();
+  cout<<q.front()<<" ";
+
 
 }
